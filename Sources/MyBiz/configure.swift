7c6822dd5d1808d5c1ef1cb181ba97188c262ad2
Edwin Cardenas <ecardenas@lwolf.com>
2026-03-02 16:01:37 -0500

    feat: add mybiz implementation

--- Sources/MyBiz/configure.swift
+++ Sources/MyBiz/configure.swift
@@ -1,6 +1,6 @@
-import NIOSSL
 import Fluent
 import FluentSQLiteDriver
+import NIOSSL
 import Vapor
 
 // configures your application
@@ -11,6 +11,20 @@ public func configure(_ app: Application) async throws {
     app.databases.use(DatabaseConfigurationFactory.sqlite(.file("db.sqlite")), as: .sqlite)
 
     app.migrations.add(CreateTodo())
+    app.migrations.add(CreateUsers())
+    app.migrations.add(SeedUsers())
+    app.migrations.add(CreateTokens())
+
+    app.migrations.add(CreateAnnouncements())
+    app.migrations.add(SeedAnnouncements())
+    app.migrations.add(CreateEmployees())
+    app.migrations.add(SeedEmployees())
+    app.migrations.add(CreateEvents())
+    app.migrations.add(SeedEvents())
+    app.migrations.add(CreateProducts())
+    app.migrations.add(SeedProducts())
+    app.migrations.add(CreatePurchases())
+    app.migrations.add(SeedPurchases())
 
     // register routes
     try routes(app)
--- Sources/MyBiz/routes.swift
+++ Sources/MyBiz/routes.swift
@@ -11,4 +11,10 @@ func routes(_ app: Application) throws {
     }
 
     try app.register(collection: TodoController())
+    try app.register(collection: AnnouncementsController())
+    try app.register(collection: UserController())
+    try app.register(collection: EmployeesController())
+    try app.register(collection: EventsController())
+    try app.register(collection: ProductsController())
+    try app.register(collection: PurchasesController())
 }
